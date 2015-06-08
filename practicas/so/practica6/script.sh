17d9a58113c4886ac64d623e905c65d568ec57ef
Raúl Benencia <rul@kalgan.cc>
2015-06-08 16:18:03 -0300

    systemd and normal service execution

--- practicas/so/practica6/script.sh
+++ practicas/so/practica6/script.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 while true; do
     clear
-    echo "Hostname: $(hostname)\n"
-    echo "/home directory contents:\n"
+    echo -e "Hostname: $(hostname)\n"
+    echo -e "/home directory contents:\n"
     ls -l /home
-    echo "\nSome processes:\n$(ps -e | tail)"
-    echo "\nProcess quantity:\n$(ps -e | wc -l)"
-    sleep 1
+    echo -e "\nSome processes:\n$(ps -e | tail)"
+    echo -e "\nProcess quantity:\n$(ps -e | wc -l)"
+    sleep 10
 done
 
